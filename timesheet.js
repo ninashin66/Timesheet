08404923c2fc75cf214a049c062b211ab00dcd6c
Colin D <colinv120@gmail.com>
2019-07-25 21:23:30 -0600

    Finsihed the on click for the submit button

--- timesheet.js
+++ timesheet.js
@@ -14,3 +14,50 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // This is to try a pull request
+var userName = "";
+var userRole = "";
+var userSD = "";
+var userMRate = "";
+
+$(".submit").on("click", () => {
+  event.preventDefault();
+  console.log("hi");
+  userName = $("#name")
+    .val()
+    .trim();
+  userRole = $("#role")
+    .val()
+    .trim();
+  userSD = $("#sDate")
+    .val()
+    .trim();
+  userMRate = $("#mRate")
+    .val()
+    .trim();
+  database.ref().push({
+    name: userName,
+    role: userRole,
+    startDate: userSD,
+    monthlyRate: userMRate
+  });
+  var myArray = [];
+
+  myArray.push(userName);
+  myArray.push(userRole);
+  myArray.push(userSD);
+  myArray.push(userMRate);
+  console.log(userName);
+  console.log(myArray);
+  function newLine(arr) {
+    var tr = $("<tr></tr>");
+    arr.forEach(function(i) {
+      var td = $("<td></td>");
+      td.text(i);
+      td.appendTo(tr);
+    });
+    return tr;
+  }
+  var myLine = newLine(myArray);
+  $("#employeeInfo").append(myLine);
+  console.log(myLine);
+});
