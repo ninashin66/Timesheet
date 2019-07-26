3be5f7b2ce0988d83a2e604b752387c7b5c20de9
Colin D <colinv120@gmail.com>
2019-07-25 19:28:13 -0600

    linked the firebase database

---
+++ timesheet.js
@@ -0,0 +1,14 @@
+// Colin's Firebase
+var config = {
+  apiKey: "AIzaSyBIZz8iZDQaMriGqtfFt6QvYrwhAJSSTvM",
+  authDomain: "fir-74cb1.firebaseapp.com",
+  databaseURL: "https://fir-74cb1.firebaseio.com",
+  projectId: "fir-74cb1",
+  storageBucket: "",
+  messagingSenderId: "465135312392",
+  appId: "1:465135312392:web:2566a9ea502046e4"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
